2f0b3a2b8b6f5b3c9022479e33c6650708f75d44
Vimal Mudalagi <vimalmudalagi123@gmail.com>
2023-12-12 10:52:16 +0530

    Added another html file

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 // To make a collapsing side bar
 var menuIcon = document.querySelector(".menu-icon");
 var sideBar = document.querySelector(".sidebar");
+var container = document.querySelector(".container");
 
-menuIcon.onclick = function () {
+menuIcon.onClick = function () {
     sideBar.classList.toggle("small-sidebar");
+    container.classList.toggle("large-container");
+
 }
